26511cd434ed556bd483daf998e69b36457f1c6b
Juan Esteban Arango Amaya <juan.arango@experimentality.co>
2022-11-30 02:40:32 -0500

    Some adjustments to the integration logger, currency processor and the currency dto

--- src/main/java/com/jblackheart/microservices/camelmicroserviceb/util/IntegrationLogger.java
+++ src/main/java/com/jblackheart/microservices/camelmicroserviceb/util/IntegrationLogger.java
@@ -2,25 +2,16 @@ package com.jblackheart.microservices.camelmicroserviceb.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Date;
-import java.util.Objects;
 
 @Component
 public class IntegrationLogger {
-    @Autowired
-    private Environment environment;
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-    private final String APP_NAME = Objects.requireNonNull(environment.getProperty(ConstantVariables.LOGGER_APP_NAME));
+    private final String APP_NAME = "camel.microservice-b";
     protected static final String linea = "=====================================================================================================";
-
-    public IntegrationLogger() {
-    }
 
     public void info(String message, String className) {
         String code = "\nCode           : -> ".concat(APP_NAME);
